3c50df36f9204689cefe14a912dc08a77589adf1
zachary-hamilton <57552986+zachary-hamilton@users.noreply.github.com>
2020-05-05 23:53:52 -0400

    init file

--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -4,8 +4,12 @@
 def enlarge(n):
     return n * 100
 
-x = 5
-print(enlarge(x))
 
-y = int(input('Please choose a number (e.g. 5)'))
-print(enlarge(y))
+if __name__ == "__main__":
+    
+    x = 5
+    print(enlarge(x))
+
+    y = int(input('Please choose a number (e.g. 5)'))
+    print(enlarge(y))
+    
--- my_lambdata/my_script.py
+++ my_lambdata/my_script.py
@@ -1,6 +1,10 @@
 
 from pandas import DataFrame
+from my_lambdata.my_mod import enlarge
+
 print("Hello")
 
 df = DataFrame({'a':[1,2,3], 'b':[4,5,6]})
 print(df.head())
+x = 11
+print(enlarge(x))
